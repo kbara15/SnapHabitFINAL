d1b6213cb93807528fc113a7e9b4d08f369f5161
Kent Bara <kbaracello15@gmail.com>
2020-01-14 13:03:23 +0900

    Initial Commit

---
+++ SnapHabitFINAL/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapHabitFINAL
+//
+//  Created by Kent Bara on 2020/01/14.
+//  Copyright © 2020 Kent Bara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
